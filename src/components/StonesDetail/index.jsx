404339910fb9ec70b1831a9e569ccbe3a6116624
admrts <alidemirtas.7@icloud.com>
2023-11-15 13:23:51 +0300

    details page

--- src/components/StonesDetail/index.jsx
+++ src/components/StonesDetail/index.jsx
@@ -1,59 +1,82 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import stones from "../../data/stonesBricks/stones.json";
 import bricks from "../../data/stonesBricks/bricks.json";
 
 const Stones = ({ category, name }) => {
-  const stonesData = stones
-    .filter((item) => item.title === category)
-    .concat(bricks.filter((item) => item.title == category));
+  const [mainImage, setMainImage] = useState();
+  const [currentStoneData, setCurrentStoneData] = useState();
+  const [stonesData, setStonesData] = useState();
 
-  const currentStoneData = stonesData[0].kind.find(
-    (item) => item.title === name
-  );
-  console.log(currentStoneData);
+  useEffect(() => {
+    const stonesList = stones
+      .filter((item) => item.title === category)
+      .concat(bricks.filter((item) => item.title == category));
+    setStonesData(stonesList);
+    const current = stonesList[0].kind.find((item) => item.title === name);
+    setCurrentStoneData(current);
+    setMainImage(current.colors[0]);
+  }, []);
+
+  const handleColorsClick = (item) => {
+    setMainImage(item);
+  };
+  const handleButtonClick = (item) => {
+    setCurrentStoneData(item);
+  };
   return (
-    <div>
-      <div className="container  details-page-container">
+    <div className="details-page-container">
+      <div className="container">
         <h4 className="row justify-content-center">{category}</h4>
         <div className="container">
-          <div className="row mt-30 mb-30 gap-3 justify-content-center align-items-center">
-            {stonesData[0].kind.map((item, i) => (
-              <div key={i} className="col btn btn-secondary w-auto ">
-                {item.title}
-              </div>
-            ))}
+          <div className=" details-page-kind-container">
+            {stonesData
+              ? stonesData[0].kind.map((item, i) => (
+                  <div
+                    key={i}
+                    onClick={() => handleButtonClick(item)}
+                    className="details-page-colors-button  "
+                  >
+                    {item.title}
+                  </div>
+                ))
+              : null}
           </div>
           <div className="row">
             <div className="text-center fs-3 my-4">{name}</div>
           </div>
         </div>
 
+        <div className="container"></div>
+
         <div className="container">
-          <div className="row">
-            <div className="col-md-1">
-              <p className="fs-3 fw-bold vertical-text text-dark">RENKLER</p>
-            </div>
-            <div className="col-md-2  border d-flex flex-column gap-3 align-items-center py-3">
-              {currentStoneData.colors.map((item, i) => (
-                <div className="shadow-lg  w-75 h-75" key={i}>
-                  <img src={item.image} alt="" />
-                  <p className="text-center fs-6 ">{item.title}</p>
-                </div>
-              ))}
-            </div>
-            <div className="col-md-6 border py-3">
-              <img src={currentStoneData.colors[0].image} alt="" />
-            </div>
-            <div className="col-md-3 border">
-              <div>Teknik Özellikler</div>
-              <div>
-                <div>Boyutlar</div>
-                <div>1m2</div>
+          <div className="row ">
+            {mainImage ? (
+              <div className="d-flex justify-content-center align-items-center">
+                <img
+                  className="details-main-image"
+                  src={mainImage.image}
+                  alt={mainImage.title}
+                />
               </div>
-              <div>Kalınlık</div>
-              <div>Derz Aralığı</div>
-            </div>
+            ) : null}
           </div>
+          <h5 className="text-center mt-100 mb-50">RENKLER</h5>
+          <div className="details-colors-container">
+            {currentStoneData
+              ? currentStoneData.colors.map((item, i) => (
+                  <div className="" key={i}>
+                    <img
+                      className="details-colors-image"
+                      src={item.image}
+                      alt={item.title}
+                      onClick={() => handleColorsClick(item)}
+                    />
+                    <div className="text-center mt-10">{item.title}</div>
+                  </div>
+                ))
+              : null}
+          </div>
+          <h6 className="text-center mt-50">Teknik Özellikler</h6>
         </div>
       </div>
     </div>
