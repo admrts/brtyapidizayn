1ff120049af0a55747943c89d6ad9fff6fe49984
admrts <alidemirtas.7@icloud.com>
2023-11-16 12:08:33 +0300

    build fix

---
+++ src/pages/celikkonstruksiyon/index.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import WorkHeader from "../../components/Work-header";
+import MainLayout from "../../layouts/main";
+import { WorkTwoColumn } from "../../components/Work-Two-Column";
+
+const Work1 = () => {
+  React.useEffect(() => {
+    document.querySelector("body").classList.add("index3");
+  }, []);
+  return (
+    <MainLayout>
+      <WorkHeader
+        title={{
+          first: "Statik Proje",
+        }}
+        // title = "text",
+        content="MİMARİ PROJEYE UYGUN OLARAK, ÖLÇEKLERİ YAPININ BÜYÜKLÜĞÜNE VE PROJENİN ÖZELLİĞİNE GÖRE BETONARME, YIĞMA, ÇELİK VE BENZERİ YAPI TÜRLERİNİN TAŞIYICI SİSTEMLERİNİ GÖSTEREN VE BUNLARIN KESİTLERİ, DETAYLARI İLE HESAPLARI TARAFIMIZCA SİZLER İÇİN YAPILMAKTADIR. MÜHENDİSLİKTE TASARIMLARIMIZ 3E KURALINA UYGUN OLARAK YAPILMAKTADIR."
+      />
+      <WorkTwoColumn />
+    </MainLayout>
+  );
+};
+
+export default Work1;
