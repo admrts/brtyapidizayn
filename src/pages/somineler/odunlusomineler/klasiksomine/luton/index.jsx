49dc9fb070a6d2ae8e9b764f92c447c762f32bda
admrts <alidemirtas.7@icloud.com>
2023-12-02 11:40:06 +0300

    fix build

--- src/pages/somineler/odunlusomineler/klasiksomine/luton/index.jsx
+++ src/pages/somineler/odunlusomineler/klasiksomine/luton/index.jsx
@@ -1,10 +1,11 @@
 import MainLayout from "../../../../../layouts/main";
 import FireplaceDetails from "../../../../../components/FireplaceDetails";
+import data from "./data.json";
 
 const Page = () => {
   return (
     <MainLayout>
-      <FireplaceDetails />
+      <FireplaceDetails data={data} />
     </MainLayout>
   );
 };
